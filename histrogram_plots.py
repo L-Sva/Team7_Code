351f3508e24f14b40c5a3d6036fe12393ed2fde8
Marek Cottingham <TheMuonNeutrino@users.noreply.github.com>
2022-01-18 07:18:39 +0000

    Histogram code comments

--- histrogram_plots.py
+++ histrogram_plots.py
@@ -17,13 +17,15 @@ def load_file(filename='total_dataset.pkl') -> pd.DataFrame:
 def plot_hist_quantity(df,column,bins=100,range=None,label=None):
     # Prevents exception being thrown for 'year' column
     if df[column].dtype != 'object':
+
+        # Generating histogram data
         h, bins = np.histogram(df[column], bins=bins, range=range)
         h_density, _ = np.histogram(df[column], bins=bins, density = True)
         half_binwidths = (bins[1] - bins[0]) / 2
         bin_centres = (bins[1:] + bins[:-1]) / 2
-        #mplhep.histplot(h, bins, yerr=True, histtype='errorbar', xerr=half_binwidths, marker='.')
         plt.errorbar(
-            x=bin_centres, y=h_density, 
+            x=bin_centres, y=h_density,
+            # Need to use this to get correct sized errorbars on a histogram with density=True
             yerr=np.sqrt(h) / h * h_density, 
             xerr=half_binwidths, capsize=3, ls='', label=label)
         plt.xlabel(column)
@@ -40,8 +42,6 @@ if __name__ == '__main__':
     for column in total_dataset:
         bins, h = plot_hist_quantity(total_dataset, column, label='total_dataset')
         plot_hist_quantity(jpsi, column, label='jpsi', bins=bins)
-        #plot_hist_quantity(signal, column, label='signal', bins = bins)
         plt.legend()
-        #plt.show()
         plt.savefig(path.join('data_histograms',f'{column}.png'))
         plt.close()
