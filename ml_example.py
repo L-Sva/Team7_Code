0b5b3d4f0e7358fca6f2251e03bc6ad9af1863d6
Marek Cottingham <36125218+marek-cottingham@users.noreply.github.com>
2022-01-24 11:53:50 +0000

    Added example of using ML to find peaking backgrounds

--- ml_example.py
+++ ml_example.py
@@ -1,8 +1,11 @@
-from xgboost import RabitTracker
+
 from core import load_file, RAWFILES
 import ml_tools
 import pandas as pd
 import xgboost
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 total_dataset = load_file(RAWFILES.TOTAL_DATASET)
 
@@ -24,11 +27,36 @@ sig_train, sig_test = ml_tools.ml_prepare_test_train(signal_dataset)
 for df in [peaking_bks_test, peaking_bks_train, sig_test, sig_train]:
     ml_tools.ml_strip_columns(df, inplace=True)
 
-xge_model = xgboost.XGBClassifier()
+train_data = ml_tools.ml_combine_signal_bk(sig_train[:10000], peaking_bks_train[:10000])
+test_data = ml_tools.ml_combine_signal_bk(sig_test, peaking_bks_test)
 
-train_data = ml_tools.ml_combine_signal_bk(sig_train, peaking_bks_train)
+xgboost.set_config(verbosity=2)
+xge_model = xgboost.XGBClassifier(
+    max_depth=4,
+    num_boost_round = 10
+)
 
-ml_tools.ml_train_model(train_data, xge_model)
 
+print('Starting model training')
+ml_tools.ml_train_model(train_data, xge_model, 
+    eval_metric='logloss',
+)
+print('Completed model training')
 
+xge_model.save_model(os.path.join('examples_save','0001_peaking.model'))
 
+sig_prob = ml_tools.ml_get_model_sig_prob(test_data, xge_model) # signal probability from bdt model
+
+threshold_list = np.sort(
+    np.concatenate((np.linspace(0.8,1,20), np.linspace(0.97,0.995, 20)))
+)
+sb_list = []
+for thresh in threshold_list:
+    sb_list.append(ml_tools.test_sb(test_data, sig_prob, thresh))
+
+plt.plot(threshold_list, sb_list)
+plt.xlabel('Cut value')
+plt.ylabel('$S / \\sqrt{S + B}$')
+plt.show()
+
+print(ml_tools.test_false_true_negative_positive(test_data, sig_prob, 0.988))
--- ml_tools.py
+++ ml_tools.py
@@ -3,7 +3,6 @@ from core import RAWFILES, load_file
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
-import matplotlib.pyplot as plt
 
 BASE_NAMES = [name for name in load_file(RAWFILES.SIGNAL)]
 
@@ -19,27 +18,25 @@ def ml_strip_columns(dataframe,
 
     # Drops 'year' and 'B0_ID' columns
     columns_names_to_drop = ('year','B0_ID')
-    for name in columns_names_to_drop:
-        dataframe = dataframe.drop(name)
 
     # Drops any columns added during processing not specified to keep
     for name in dataframe:
         if (
             not (name in BASE_NAMES or name in accepted_column_names or name == 'category')
-            or name in reject_column_names
+            or name in reject_column_names or name in columns_names_to_drop
         ):
-            dataframe.drop(name, inplace=True)
+            dataframe.drop(name, inplace=True, axis=1)
 
     return dataframe
 
-def ml_train_model(training_data, model):
+def ml_train_model(training_data, model, **kwargs):
     """Trains a ML model. Requires that the parameter `training_data` contains a column named 'category'
     which will be the value the ML model is trained to predict; this should contain only integers,
     preferably only 0 or 1.
     """
 
-    train_vars = training_data.drop('category')
-    model.fit(train_vars, training_data['category'])
+    train_vars = training_data.drop('category',axis=1)
+    model.fit(train_vars, training_data['category'].to_numpy(), **kwargs)
     return model
 
 def ml_prepare_test_train(dataset, randomiser_seed = 1) -> Tuple[pd.DataFrame, pd.DataFrame]:
@@ -53,19 +50,22 @@ def ml_combine_signal_bk(signal_dataset, background_dataset):
     """Combines signal and background dataset, adding category labels
     """
     # Marek
-    signal_dataset['category'] = 1
-    background_dataset['category'] = 1
+    signal_dataset = signal_dataset.copy()
+    background_dataset = background_dataset.copy()
+    signal_dataset.loc[:,'category'] = 1
+    background_dataset.loc[:,'category'] = 0
 
     # combine
     dataset = pd.concat((signal_dataset, background_dataset))
     return dataset
 
+def ml_get_model_sig_prob(testData, model):
+    if 'category' in testData:
+        test_vars = testData.drop('category',axis=1)
+    return model.predict_proba(test_vars)[:,1]
 
-def test_false_true_negative_positive(model, test_dataset, threshold) -> dict:
+def test_false_true_negative_positive(test_dataset, sig_prob, threshold) -> dict:
     # Jiayang
-
-    sig_prob = model # signal probability from bdt model
-
 
     signal = 0
     background = 0
@@ -74,17 +74,18 @@ def test_false_true_negative_positive(model, test_dataset, threshold) -> dict:
     false_positive = 0
     true_negative = 0
 
-    for i in range(len(test_dataset['catagory'])):
-        if (test_dataset['catagory'][i] == 1) and (sig_prob[i] >= threshold): # signal + postive
+    x = test_dataset['category'].to_numpy()
+    for i in range(len(x)):
+        if (x[i] == 1) and (sig_prob[i] >= threshold): # signal + postive
             signal += 1
             true_positive += 1
-        elif (test_dataset['catagory'][i] == 1) and (sig_prob[i] < threshold): # signal + negative
+        elif (x[i] == 1) and (sig_prob[i] < threshold): # signal + negative
             signal += 1
             false_negtive += 1
-        elif (test_dataset['catagory'][i] == 0) and (sig_prob[i] >= threshold): # background + postive
+        elif (x[i] == 0) and (sig_prob[i] >= threshold): # background + postive
             background += 1
             false_positive +=1
-        elif (test_dataset['catagory'][i] == 0) and (sig_prob[i] < threshold): # background + negative
+        elif (x[i] == 0) and (sig_prob[i] < threshold): # background + negative
             background += 1
             true_negative +=1
 
@@ -118,10 +119,10 @@ def plot_roc_curve():
     # Jose
     pass
 
-def test_sb(model, test_dataset, threshold):
+def test_sb(test_dataset, sig_prob, threshold):
     # Jiayang
 
-    output = test_false_true_negative_positive(model, test_dataset, threshold)
+    output = test_false_true_negative_positive(test_dataset, sig_prob, threshold)
 
     S = output['signal'] * output['true-positive']
     B = output['background'] * output['false-positive']
