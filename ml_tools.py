55f7512730b1844927f5d112bf0035af7bfe1f80
Marek Cottingham <36125218+marek-cottingham@users.noreply.github.com>
2022-01-26 12:46:03 +0000

    Added changes by Gefaell

--- ml_tools.py
+++ ml_tools.py
@@ -3,8 +3,9 @@ from core import RAWFILES, load_file
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
-from sklearn import metrics
 import matplotlib.pyplot as plt
+from sklearn.metrics import auc, roc_curve
+from xgboost import XGBClassifier
 
 BASE_NAMES = [name for name in load_file(RAWFILES.SIGNAL)]
 
@@ -20,25 +21,27 @@ def ml_strip_columns(dataframe,
 
     # Drops 'year' and 'B0_ID' columns
     columns_names_to_drop = ('year','B0_ID')
+    for name in columns_names_to_drop:
+        dataframe = dataframe.drop(name)
 
     # Drops any columns added during processing not specified to keep
     for name in dataframe:
         if (
             not (name in BASE_NAMES or name in accepted_column_names or name == 'category')
-            or name in reject_column_names or name in columns_names_to_drop
+            or name in reject_column_names
         ):
-            dataframe.drop(name, inplace=True, axis=1)
+            dataframe.drop(name, inplace=True)
 
     return dataframe
 
-def ml_train_model(training_data, model, **kwargs):
+def ml_train_model(training_data, model):
     """Trains a ML model. Requires that the parameter `training_data` contains a column named 'category'
     which will be the value the ML model is trained to predict; this should contain only integers,
     preferably only 0 or 1.
     """
 
-    train_vars = training_data.drop('category',axis=1)
-    model.fit(train_vars, training_data['category'].to_numpy(), **kwargs)
+    train_vars = training_data.drop('category')
+    model.fit(train_vars, training_data['category'])
     return model
 
 def ml_prepare_test_train(dataset, randomiser_seed = 1) -> Tuple[pd.DataFrame, pd.DataFrame]:
@@ -52,36 +55,40 @@ def ml_combine_signal_bk(signal_dataset, background_dataset):
     """Combines signal and background dataset, adding category labels
     """
     # Marek
-    signal_dataset = signal_dataset.copy()
-    background_dataset = background_dataset.copy()
-    signal_dataset.loc[:,'category'] = 1
-    background_dataset.loc[:,'category'] = 0
+    signal_dataset['category'] = 1
+    background_dataset['category'] = 1
 
     # combine
     dataset = pd.concat((signal_dataset, background_dataset))
     return dataset
 
-def ml_get_model_sig_prob(testData, model):
-    if 'category' in testData:
-        test_vars = testData.drop('category',axis=1)
-    return model.predict_proba(test_vars)[:,1]
 
-def test_false_true_negative_positive(test_dataset, sig_prob, threshold) -> dict:
+def test_false_true_negative_positive(model, test_dataset, threshold) -> dict:
     # Jiayang
 
-    x = test_dataset['category'].to_numpy()
+    sig_prob = model # signal probability from bdt model
 
-    x_mask_0 = x == 0
-    x_mask_1 = x == 1
-    prb_mask_pos = sig_prob >= threshold
-    prb_mask_neg = sig_prob < threshold
 
-    signal = np.count_nonzero(x_mask_1)
-    background = np.count_nonzero(x_mask_0)
-    true_positive =  np.count_nonzero(np.logical_and(x_mask_1, prb_mask_pos))
-    false_negative = np.count_nonzero(np.logical_and(x_mask_1, prb_mask_neg))
-    false_positive = np.count_nonzero(np.logical_and(x_mask_0, prb_mask_pos))
-    true_negative =  np.count_nonzero(np.logical_and(x_mask_0, prb_mask_neg))
+    signal = 0
+    background = 0
+    true_positive = 0
+    false_negtive = 0
+    false_positive = 0
+    true_negative = 0
+
+    for i in range(len(test_dataset['catagory'])):
+        if (test_dataset['catagory'][i] == 1) and (sig_prob[i] >= threshold): # signal + postive
+            signal += 1
+            true_positive += 1
+        elif (test_dataset['catagory'][i] == 1) and (sig_prob[i] < threshold): # signal + negative
+            signal += 1
+            false_negtive += 1
+        elif (test_dataset['catagory'][i] == 0) and (sig_prob[i] >= threshold): # background + postive
+            background += 1
+            false_positive +=1
+        elif (test_dataset['catagory'][i] == 0) and (sig_prob[i] < threshold): # background + negative
+            background += 1
+            true_negative +=1
 
     # sanity check
     # total = true_positive + false_negtive + false_positive + true_negative
@@ -92,7 +99,7 @@ def test_false_true_negative_positive(test_dataset, sig_prob, threshold) -> dict
     tpr = true_positive / signal
     fpr = false_positive / background
 
-    fnr = false_negative / signal
+    fnr = false_negtive / signal
     tnr = true_negative / background
 
     return {
@@ -105,25 +112,25 @@ def test_false_true_negative_positive(test_dataset, sig_prob, threshold) -> dict
     }
 
 
-def roc_curve(test_data, sp):
+def roc_function(sp, test_data):
     # Jose
-    # Why the use of .head() on the fourth line of this docstring?
     '''
     Implement the following model before this function:
         model = XGBClassifier()
         model.fit(training_data[training_columns], training_data['category'])
-        model.predict_proba(test_data[training_columns].head())
+        sp = model.predict_proba(test_data[training_columns])[:,1]
+        model.predict_proba(test_data[training_columns])
     This returns an array of N_samples by N_classes. 
     The first column is the probability that the candiate is category 0 (background).
-    The second column is the probability that the candidate is category 1 (signal).
+    The second column (sp) is the probability that the candidate is category 1 (signal).
 
     The Receiver Operating Characteristic curve given by this function shows the efficiency of the classifier 
     on signal (true positive rate, tpr) against the inefficiency of removing background (false positive 
     rate, fpr). Each point on this curve corresponds to a cut value threshold.
     '''
 
-    fpr, tpr, cut_values = metrics.roc_curve(test_data['category'], sp)
-    area = metrics.auc(fpr, tpr)
+    fpr, tpr, cut_values = roc_curve(test_data['category'], sp)
+    area = auc(fpr, tpr)
     
     return {
         'fpr': fpr,
@@ -145,10 +152,10 @@ def plot_roc_curve(fpr, tpr, area):
     plt.gca().set_aspect('equal', adjustable='box')
     #plt.show()
 
-def test_sb(test_dataset, sig_prob, threshold):
+def test_sb(model, test_dataset, threshold):
     # Jiayang
 
-    output = test_false_true_negative_positive(test_dataset, sig_prob, threshold)
+    output = test_false_true_negative_positive(model, test_dataset, threshold)
 
     S = output['signal'] * output['true-positive']
     B = output['background'] * output['false-positive']
