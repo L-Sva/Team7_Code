5e46e657f6a1bdc9682b5ca4c349783b01e8611d
Marek Cottingham <36125218+marek-cottingham@users.noreply.github.com>
2022-02-05 12:05:34 +0000

    Catching warnings

--- histrogram_plots.py
+++ histrogram_plots.py
@@ -1,3 +1,4 @@
+import warnings
 import numpy as np
 import matplotlib.pyplot as plt
 import os.path as path
@@ -10,21 +11,26 @@ def plot_hist_quantity(df,column,bins=100,range=None,label=None):
     if df[column].dtype != 'object':
 
         # Generating histogram data
-        h, bins = np.histogram(df[column], bins=bins, range=range)
-        h_density, _ = np.histogram(df[column], bins=bins, density = True)
-        half_binwidths = (bins[1] - bins[0]) / 2
-        bin_centres = (bins[1:] + bins[:-1]) / 2
-        plt.errorbar(
-            x=bin_centres, y=h_density,
-            # Need to use this to get correct sized errorbars on a histogram with density=True
-            # yerr = 0 for h = 0
-            yerr= np.divide(np.sqrt(h), h, out = np.zeros(h.shape, dtype='float'), where = h!=0) * h_density, 
-            xerr=half_binwidths, capsize=3, ls='', label=label)
+        with warnings.catch_warnings():
+            warnings.simplefilter(action='ignore')
+            h, bins = np.histogram(df[column], bins=bins, range=range)
+            h_density, _ = np.histogram(df[column], bins=bins, density = True)
+            half_binwidths = (bins[1] - bins[0]) / 2
+            bin_centres = (bins[1:] + bins[:-1]) / 2
+            yerr = np.divide(np.sqrt(h), h, out = np.zeros(h.shape, dtype='float'), where = h!=0) * h_density
+            plt.errorbar(
+                x=bin_centres, y=h_density,
+                # Need to use this to get correct sized errorbars on a histogram with density=True
+                # yerr = 0 for h = 0
+                yerr= yerr,
+                xerr=half_binwidths, capsize=3, ls='', label=label)
         plt.xlabel(column)
         plt.ylabel('Probability density')
         plt.xlim(bins[0], bins[-1])
 
         return bins, h
+    else:
+        warnings.warn(f'Cannot plot column {column}, as it has dtype of `object`')
 
 def generic_selector_plot(orginal,subset, not_subset, column, bins = 100, show = True):
     bins, h = plot_hist_quantity(orginal, column, label='Original', bins=bins)
