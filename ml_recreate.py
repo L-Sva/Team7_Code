1b759db3cecbb3cf9001c0b0bd0747f8808f33b6
Marek Cottingham <36125218+marek-cottingham@users.noreply.github.com>
2022-02-07 09:29:09 +0000

    Simplified data import routine for ml

--- ml_recreate.py
+++ ml_recreate.py
@@ -1,9 +1,10 @@
-from typing import Tuple
 
+from typing import Tuple
 from pandas import DataFrame
+import scipy
 import xgboost
-from core import load_file, RAWFILES
-from histrogram_plots import plot_hist_quantity
+from core import ensure_dir, load_file, RAWFILES
+from histrogram_plots import generic_selector_plot, plot_hist_quantity
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -11,15 +12,23 @@ import os
 
 import ml_tools
 
-def load_train_test(file, test_size=0.2):
+def load_train_validate_test(file, test_size=0.2, validate_size=0.16):
     data = load_file(file)
     data = data.drop('year',axis=1)
-    m = int(
-        len(data) * (1-test_size)
-    )
-    return data[0:m], data[m:]
+    if test_size == 0:
+        return data
+    else:
+        m = int(
+            len(data) * (1-test_size)
+        )
+        if validate_size == 0:
+            return data[0:m], data[m:]
+        else:
+            p = int(m * (1-validate_size))
+            return data[0:p], data[p:m], data[m:]
+        
 
-def combine_datasets(signal: Tuple[DataFrame],background: Tuple[DataFrame]):
+def combine_signal_background(signal: Tuple[DataFrame],background: Tuple[DataFrame]):
     res = []
     for i, (s,b) in enumerate(zip(signal,background)):
         s = s.copy()
@@ -27,14 +36,41 @@ def combine_datasets(signal: Tuple[DataFrame],background: Tuple[DataFrame]):
         s.loc[:,'category'] = 1
         b.loc[:,'category'] = 0
         combine = pd.concat((s,b), ignore_index=True)
-        if i == 1:
+        if i >= 1:
             combine = pd.concat((b,s), ignore_index=True)
         combine = combine.sample(frac=1, axis=0, random_state=1)
         combine = combine.reset_index(drop=True)
         res.append(combine)
     return res
 
-def fit_new_model(train_data):
+def concat_datasets(datasets):
+    res = []
+    for to_combine in zip(*datasets):
+        combine = pd.concat(to_combine, ignore_index=True)
+        res.append(combine)
+    return res
+
+def fit_new_model(train_data, **params):
+    default_params = {
+        'n_estimators': 400,
+        'subsample': 1,
+        'max_depth': 6,
+        'learning_rate': 0.05,
+        'gamma': 0,
+        'reg_alpha': 1,
+        'reg_lambda': 2,
+    }
+    for key, value in params.items():
+        default_params[key] = value
+    model = xgboost.XGBClassifier(use_label_encoder=False,**default_params)
+    model.fit(
+        train_data.drop('category',axis=1).values,
+        train_data['category'].to_numpy().astype('int'),
+        eval_metric='logloss'
+    )
+    return model
+
+def load_model_file(path):
     params = {
         'n_estimators': 400,
         'subsample': 1,
@@ -45,38 +81,106 @@ def fit_new_model(train_data):
         'reg_lambda': 2,
     }
     model = xgboost.XGBClassifier(use_label_encoder=False,**params)
-    model.fit(
-        train_data.drop('category',axis=1).values,
-        train_data['category'].to_numpy().astype('int'),
-        eval_metric='logloss'
-    )
+    model.load_model(path)
     return model
 
+def select(data, model, thresh):
+    if 'category' in data:
+        data = data.drop('category',axis=1)
+    sig_prob = model.predict_proba(data.values)[:,1]
+    accept = sig_prob > thresh
+    s = data[accept]
+    ns = data[~accept]
+    return s, ns
 
-signal = load_train_test(RAWFILES.SIGNAL)
-background = load_train_test(RAWFILES.JPSI)
+def make_selector(model, thresh):
+    def generated_selector(data):
+        return select(data, model, thresh)
+    return generated_selector
 
-combine = combine_datasets(signal, background)
+def plot_sb(data, sig_prob):
+    thresh_list = np.linspace(0.1, 1, 600)
+    sb = [
+        ml_tools.test_sb(data,sig_prob, t) for t in thresh_list
+    ]
+    plt.plot(thresh_list,sb)
 
-model = fit_new_model(combine[0][:30000])
-model.save_model(os.path.join('examples_save','0006_peak.model'))
-sig_prob = model.predict_proba(combine[1].drop('category',axis=1).values)[:,1]
+def optimize_threshold(validate_dataset, sig_prob):
+    def target_func(x):
+        return 400-ml_tools.test_sb(validate_dataset, sig_prob, x)
 
-print(ml_tools.test_false_true_negative_positive(combine[1], sig_prob, 0.95))
-thresh = np.linspace(0.1, 1, 600)
-sb = [
-    ml_tools.test_sb(combine[1],sig_prob, t) for t in thresh
-]
-plt.plot(thresh,sb)
-plt.figure()
+    optimize_result = scipy.optimize.dual_annealing(target_func,bounds=((0,1),),x0=[0.95])
+    thresh = optimize_result.x[0]
+    return thresh
 
-bins, h = plot_hist_quantity(signal[1], 'q2')
-plot_hist_quantity(background[1], 'q2',bins=bins)
-plot_hist_quantity(combine[1][combine[1]['category'] == 0],'q2',bins)
+def predict_prob(data, model):
+    return model.predict_proba(data.drop('category',axis=1).values)[:,1]
 
-# with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-#     print(combine[1].head())
+if __name__ == '__main__':
 
-# print(sig_prob)
+    signal = load_train_validate_test(RAWFILES.SIGNAL, validate_size=0)
+    background = load_train_validate_test(RAWFILES.JPSI, validate_size=0)
+    background = concat_datasets([load_train_validate_test(file) for file in RAWFILES.peaking_bks])
+    train, test = combine_signal_background(signal, background)
 
-plt.show()
+
+    MODEL_PATH = os.path.join('examples_save','0008_peak.model')
+
+    if not os.path.exists(MODEL_PATH):
+        model = fit_new_model(train[:30000])
+        model.save_model(MODEL_PATH)
+    else:
+        model = load_model_file(MODEL_PATH)
+
+    sig_prob = predict_prob(test, model)
+    thresh = optimize_threshold(test, sig_prob)
+
+    print(ml_tools.test_false_true_negative_positive(test, sig_prob, thresh))
+    plot_sb(test, sig_prob)
+    plt.close()
+
+    selector = make_selector(model, thresh)
+    s, ns = selector(background[0])
+    print(len(s), len(ns))
+
+    IMAGE_OUTPUT_DIR = 'data_ml_selectors_histograms'
+
+    ensure_dir(IMAGE_OUTPUT_DIR)
+
+    total = load_train_validate_test(RAWFILES.TOTAL_DATASET, test_size = 0, validate_size= 0)
+    signal_all = load_train_validate_test(RAWFILES.SIGNAL, test_size = 0, validate_size= 0)
+    s, ns = selector(total)
+    for column in total:
+        bins, h = plot_hist_quantity(total, column, label='Total dataset', bins=150)
+        plot_hist_quantity(s, column, label='ML - Signal', bins = bins)
+        plot_hist_quantity(ns, column, label='ML - Background', bins=bins)
+        plot_hist_quantity(signal_all, column, label='Simulated signal', bins=bins)
+        plt.legend()
+
+        plt.savefig(
+            os.path.join(IMAGE_OUTPUT_DIR,f'{column}.png')
+        )
+        plt.close()
+
+    DIR ='ml_hist_individual_bks'
+    OUTPUT_PLOTS = False
+    ensure_dir(DIR)
+
+    for file in RAWFILES.peaking_bks:
+        IMG_DIR = os.path.join(DIR, file[:-4])
+        _, test = load_train_validate_test(file, validate_size=0)
+        s, ns = selector(test)
+        num = len(test)
+        print(f'{file} | accepted: {len(s)} ({len(s)/num}), rejected {len(ns)} ({len(ns)/num})')
+        if OUTPUT_PLOTS:
+            cols = [col for col in test]
+            N = len(cols)
+            i = 1
+            for col in test:
+                generic_selector_plot(test, s, ns, col, 150, False)
+                plt.title(f'ML peaking bk removal on {file[:-4]}')
+                plt.savefig(os.path.join(IMG_DIR, f'{file[:-4]}_{col}.png'))
+                plt.close()
+                print(f'Outputing plots for {file} | {i} / {N}', end='\r')
+                i += 1
+            print('    Done: ')
--- ml_train.py
+++ ml_train.py
@@ -1,16 +1,14 @@
-import ml_load
+
+from ml_recreate import fit_new_model, load_train_validate_test, concat_datasets, combine_signal_background, optimize_threshold, predict_prob
+from core import RAWFILES
 import ml_tools, ml_combinatorial_extraction
-from pyexpat import model
-import xgboost
-import json
 import os
-import numpy as np
 
 """
 train and validate models using params from BayesianOptimization
 """
 
-TRAIN_COMB_BK = True
+TRAIN_COMB_BK = False
 
 SAVE_FOLDER = 'optimisation_models'
 if TRAIN_COMB_BK:
@@ -24,11 +22,11 @@ def ml_train_validate_combinatorial(**hyperparams):
 
 def ml_train_validate_peaking(**hyperparams):
     # 1. get data
-    train_data, validate_data, test_data = (
-        ml_load.get_train_validate_test_for_all_peaking_bks(train_samples_limit=None)
-    )
+    signal = load_train_validate_test(RAWFILES.SIGNAL)
+    background = concat_datasets([load_train_validate_test(file) for file in RAWFILES.peaking_bks])
+    train, validate, test = combine_signal_background(signal, background)
 
-    return ml_train_validate(train_data, validate_data, **hyperparams)
+    return ml_train_validate(train, validate, **hyperparams)
 
 
 def ml_train_validate(train_data, validate_data, **hyperparams, ):
@@ -36,33 +34,19 @@ def ml_train_validate(train_data, validate_data, **hyperparams, ):
     hyperparams['n_estimators'] = int(hyperparams['n_estimators'] )
     hyperparams['max_depth'] = int(hyperparams['max_depth'])
 
-    # 2. settings
-    xgboost.set_config(verbosity=2)
-    xge_model = xgboost.XGBClassifier(**hyperparams)
-
     # 3. train model
-    print('Starting model training')
-    # Some other model params are passed here
-    ml_tools.ml_train_model(train_data, xge_model,
-        eval_metric='logloss',
-    )
-    print('Completed model training')
+    xgb_model = fit_new_model(train_data, **hyperparams)
 
     # 4. check with validate data
-    sig_prob = ml_tools.ml_get_model_sig_prob(validate_data, xge_model)
-
+    sig_prob = predict_prob(validate_data, xgb_model)
+    thresh = optimize_threshold(validate_data, sig_prob)
+    
     # best sb model can give
     # Jose
-    threshold_list = np.linspace(0.7,1,600)
-    sb_list = []
-    for thresh in threshold_list:
-        sb_list.append(ml_tools.test_sb(validate_data, sig_prob, thresh))
-    # Finding best value of threshold to optimise SB metric
-    bestIx = np.nanargmax(np.array(sb_list))
-    bestSb = sb_list[bestIx]
-    print('sb', bestSb)
-
-    print('best sb we get possibly get:', )
+    bestSb = ml_tools.test_sb(validate_data, sig_prob, thresh)
+    print('Model SB:', bestSb)
+    print('Best thresh:', thresh)
+    print('Model stats:', ml_tools.test_false_true_negative_positive(validate_data, sig_prob, thresh))
 
     # 5. save model
     MODEL_FILE_NAME = 'peaking_sb_{}_'.format(bestSb)
@@ -72,44 +56,37 @@ def ml_train_validate(train_data, validate_data, **hyperparams, ):
         MODEL_FILE_NAME = MODEL_FILE_NAME + str(i) +'_'+ str(hyperparams[i]) +'_'
     MODEL_FILE_NAME = MODEL_FILE_NAME + '.model'
 
-    xge_model.save_model(os.path.join(SAVE_FOLDER,MODEL_FILE_NAME))
+    xgb_model.save_model(os.path.join(SAVE_FOLDER,MODEL_FILE_NAME))
 
     return bestSb
 
 
-# examples
-# bounded region of hyperparameters - arbitrary
-pbounds = {
-    'n_estimators':(100,500),
-    'subsample':(0.5,1),
-    'max_depth':(3,8),
-    'learning_rate':(0.01, 0.3),
-    'gamma':(0,0.02),
-    'reg_alpha':(0,3),
-    'reg_lambda':(1,4),
-    }
+if __name__ == '__main__':
+    # bounded region of hyperparameters - arbitrary
+    pbounds = {
+        'n_estimators':(100,500),
+        'max_depth':(3,8),
+        'learning_rate':(0.01, 0.3),
+        'gamma':(0,0.02),
+        'reg_alpha':(0,3),
+        'reg_lambda':(1,4),
+        }
 
-# bayesian_nextpoint -- example 1
-# util_args = {'kind':"ucb", 'kappa':2.5,'xi':0.0}
-# next_point = ml_tools.bayesian_nextpoint(ml_train_validate_to_be_optimized, pbounds, random_state=None, **util_args)
-# ml_train_validate_to_be_optimized(**next_point)  # train model
+    # bayesian_optimisation
+    if TRAIN_COMB_BK:
+        train_func = ml_train_validate_combinatorial
+    else:
+        train_func = ml_train_validate_peaking
 
-# bayesian_optimisation -- example 2
-if TRAIN_COMB_BK:
-    train_func = ml_train_validate_combinatorial
-else:
-    train_func = ml_train_validate_peaking
+    optimizer = ml_tools.bayesian_optimisation(
+        train_func,
+        pbounds,
+        log_folder = os.path.join(SAVE_FOLDER),
+        bool_load_logs = True,
+        explore_runs = 5, exploit_runs = 20
+        )
 
-optimizer = ml_tools.bayesian_optimisation(
-    train_func,
-    pbounds,
-    log_folder = os.path.join(SAVE_FOLDER),
-    bool_load_logs = True,
-    explore_runs = 12, exploit_runs = 100
-    #explore_runs = 0, exploit_runs = 0
-    )
+    for i, res in enumerate(optimizer.res):
+        print("Iteration {}: \n\t{}".format(i, res))
 
-for i, res in enumerate(optimizer.res):
-    print("Iteration {}: \n\t{}".format(i, res))
-
-print('Best performance:', optimizer.max)
+    print('Best performance:', optimizer.max)
