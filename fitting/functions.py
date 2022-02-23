3f8b2cf94e4ff82296e11642353f0063d9e844d1
Shiva Pingle <>
2022-02-23 19:03:45 +0000

    Added folder fitting with vectorised acceptance and fitting functions

---
+++ fitting/functions.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from numpy.polynomial import Legendre
+
+q2bins = np.array(
+    [[0.1, 0.98],
+     [1.1, 2.5],
+     [2.5, 4.0],
+     [4.0, 6.0],
+     [6.0, 8.0],
+     [15.0, 17.0],
+     [17.0, 19.0],
+     [11.0, 12.5],
+     [1.0, 6.0],
+     [15.0, 17.9]]
+)
+
+def q2_binned(df):
+    # bins ordered different, because np.histogram needs a
+    # monotonically increasing array
+    q2_bins_0 = [0.1, 0.98, 1.1, 2.5, 4, 6, 8, 15, 17, 19]
+    q2_bins_1 = [1, 6, 11, 12.5, 15, 17.9]
+
+    binned_0 = pd.cut(
+        df['q2'], bins=q2_bins_0,
+        labels=['0', 'i1', '1', '2', '3', '4', 'i2', '5', '6']
+    )
+    binned_1 = pd.cut(
+        df['q2'], bins=q2_bins_1,
+        labels=['8', 'i3', '7', 'i4', '9']
+    )
+
+    df_q2_bins_0 = dict(tuple(df.groupby(binned_0)))
+    df_q2_bins_1 = dict(tuple(df.groupby(binned_1)))
+
+    df_q2_binned = {**df_q2_bins_0, **df_q2_bins_1}
+
+    return df_q2_binned
+
+def calc_ctl_bins(acceptance_func_dfs, q2_eval, ctl_percentile):
+    # used to choose ctl bins by percentiles
+    eval_binned = pd.cut(
+        acceptance_func_dfs['q2'], bins=q2_eval,
+        labels=[f'e{num}' for num in range(len(q2_eval)-1)] # e0, e1, e2, ...
+    )
+    af_q2_eval = dict(tuple(acceptance_func_dfs.groupby(eval_binned)))
+
+    ctl_inner_bins = [
+        np.percentile(af_q2_eval[q2_bins]['costhetal'], ctl_percentile) 
+        for q2_bins in af_q2_eval
+    ]
+
+    ctl_inner_bins = np.asarray(ctl_inner_bins)
+    ones_column = np.ones((ctl_inner_bins.shape[0], 1))
+    ctl_bins = np.hstack((-ones_column, ctl_inner_bins, ones_column))
+    
+    return ctl_bins
+
+def calc_ebins_cnt(df, q2_eval, ctl_bins):
+    df_ebins = pd.cut(
+        df['q2'], bins=q2_eval,
+        labels=[f'e{num}' for num in range(len(q2_eval)-1)]
+    )
+    df_ebins = dict(tuple(df.groupby(df_ebins)))
+
+    df_cnt = np.array([np.histogram(
+        df_ebins[f'e{bin_no}']['costhetal'], bins=ctl_bins[bin_no])[0]
+        for bin_no in range(len(q2_eval)-1)])
+        
+    return df_cnt
+
+def rescale_q2(q2_array):
+    return (q2_array-9.55)/9.45
+
+def make_Leg(poly_degree):
+    P = []
+    for i in range(poly_degree):
+        Leg_int_coeff = np.zeros(i+1)
+        Leg_int_coeff[-1] = 1
+        P.append(Legendre(Leg_int_coeff))
+    return P
+
+def acceptance_function(q2, ctl, params_dict):
+    # extract required values from params_dict
+    P = params_dict['P']
+    c = params_dict['c']
+    # we can also just use params_dict['i_range'], but c found using these
+    # anyway, so we just use c's shape
+    i_range = c.shape[0]
+    j_range = c.shape[1]
+    
+    q2 = np.reshape(q2, (1, -1))
+    ctl = np.reshape(ctl, (1, -1))
+
+    P_i = np.array([P[i](q2) for i in range(i_range)])[...,None]
+    P_j = np.array([P[j](ctl) for j in range(j_range)])
+
+    func_val = (c.reshape(i_range, j_range, 1, 1) * (P_i*P_j)).sum((0, 1))
+    return func_val
+
---
+++ fitting/main.py
@@ -0,0 +1,169 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from iminuit import Minuit
+
+from Team7_Code.core import load_file, RAWFILES
+from Team7_Code.ES_functions.Compiled import selection_all
+from functions import q2_binned, acceptance_function, q2bins, rescale_q2
+
+plt.rcParams['font.size'] = 18
+
+def d2gamma_p_d2q2_dcostheta(fl, afb, cos_theta_l):
+    """
+    Returns the pdf defined above
+    :param fl: f_l observable
+    :param afb: a_fb observable
+    :param cos_theta_l: cos(theta_l)
+    :return:
+    """
+    ctl = cos_theta_l
+    c2tl = 2 * ctl ** 2 - 1
+    acceptance = acceptance_function(q0_norm[0], ctl, params_dict) 
+    scalar_array = 3/8 * (3/2 - 1/2 * fl + 1/2 * c2tl * (1 - 3 * fl) +
+                   8/3 * afb * ctl) * acceptance
+    # normalising scalar array to account for the non-unity acceptance function
+    normalised_scalar_array = scalar_array * 2 
+    return normalised_scalar_array
+
+def log_likelihood(fl, afb, _bin):
+    """
+    Returns the negative log-likelihood of the pdf defined above
+    :param fl: f_l observable
+    :param afb: a_fb observable
+    :param _bin: number of the bin to fit
+    :return:
+    """
+    
+    df = q2_filtered
+    
+    _bin = df[str(int(_bin))]
+    ctl = _bin['costhetal'].to_numpy()
+    normalised_scalar_array = d2gamma_p_d2q2_dcostheta(
+        fl=fl, afb=afb, cos_theta_l=ctl)
+    with np.errstate(invalid='ignore'): # ignore 'invalid log' message
+        NLL = -np.log(normalised_scalar_array).sum()
+    return NLL
+
+
+with open('tmp/acceptance_coeff.pkl', 'rb') as f:
+    params_dict = pickle.load(f)
+
+# q² bins to evaluate the acceptance function at
+q2_bins_mid = (q2bins[:,:-1]+q2bins[:,1:])/2
+q0_norm = rescale_q2(q2_bins_mid).flatten()
+
+raw_total = load_file(RAWFILES.TOTAL_DATASET)
+filtered_total, _ = selection_all(raw_total)
+
+q2_filtered = q2_binned(filtered_total)
+
+
+_test_bin = 1
+
+#following values are used just for fixing one parameter while the other is varied
+_test_afb = 0.7
+_test_fl = 0.0
+
+x = np.linspace(-1, 1, 500)
+fig, (ax1, ax2) = plt.subplots(1, 2)
+#testing log_likelihood for different values of fl from -1 to 1
+ax1.plot(x, [log_likelihood(fl=i, afb=_test_afb, _bin=_test_bin) for i in x]) 
+ax1.set_title(r'$A_{FB}$ = ' + str(_test_afb))
+ax1.set_xlabel(r'$F_L$')
+ax1.set_ylabel(r'$-\mathcal{L}$')
+ax1.grid()
+
+#testing log_likelihood for different values of afb from -1 to 1
+ax2.plot(x, [log_likelihood(fl=_test_fl, afb=i, _bin=_test_bin) for i in x]) 
+ax1.set_title(r'$A_{FB}$ = ' + str(_test_afb))
+ax2.set_title(r'$F_{L}$ = ' + str(_test_fl))
+ax2.set_xlabel(r'$A_{FB}$')
+ax2.set_ylabel(r'$-\mathcal{L}$')
+ax2.grid()
+plt.tight_layout()
+plt.show()
+
+
+#these 2 are only used for checking in the next cell
+bin_number_to_check = 0  # bin that we want to check in more details in the next cell
+bin_results_to_check = None
+
+
+log_likelihood.errordef = Minuit.LIKELIHOOD #set to a negative log likelihood function
+decimal_places = 3 #only used for outputting results
+starting_point = [-0.1,0.0] #starting for fl and afb respectively
+
+#setting up lists for outputting of results
+fls, fl_errs = [], []
+afbs, afb_errs = [], []
+
+for i in range(10):
+    m = Minuit(log_likelihood, fl=starting_point[0], afb=starting_point[1], _bin=i)
+    m.fixed['_bin'] = True  # fixing the bin number as we don't want to optimize it
+    m.limits=((-1.0, 1.0), (-1.0, 1.0), None) #setting the limits for parameters fl, afb, and bin number in that order
+    m.migrad() #find min using gradient descent
+    m.hesse() #finds estimation of errors at min point by looking at th
+    if i == bin_number_to_check:
+        bin_results_to_check = m #sets up results for checking in the next cell
+        
+    fls.append(m.values[0])
+    afbs.append(m.values[1])
+    fl_errs.append(m.errors[0])
+    afb_errs.append(m.errors[1])
+    
+    print(f"Bin {i}: {np.round(fls[i], decimal_places)} pm {np.round(fl_errs[i], decimal_places)},", f"{np.round(afbs[i], decimal_places)} pm {np.round(afb_errs[i], decimal_places)}. Function minimum considered valid: {m.fmin.is_valid}")
+
+fits = {'fls': fls, 'fl_errs': fl_errs, 'afbs': afbs, 'afb_errs': afb_errs}
+
+with open('tmp/af_fitting.pkl', 'wb') as f:
+    pickle.dump(fits, f)
+
+
+# actual (given values), used for comparison
+FL_aval = [
+    0.2964476598667644, 0.7603956395622157, 0.7962654932624427, 
+    0.7112903962261427, 0.6069651454293227, 0.3484407559729121, 
+    0.32808100221720543, 0.4351903815658659, 0.7476437141490421, 
+    0.34015604925198045
+]
+FL_aerr = [
+    0.05064238707746488, 0.04317410278778188, 0.03363967369234135, 
+    0.04909550894620607, 0.050785046867782326, 0.03554031324502086, 
+    0.02798976439727086, 0.03663667651413794, 0.03988961460715321, 
+    0.024825565828724583
+]
+AFB_aval = [
+    -0.0970515604684916, -0.13798671499985016, -0.017385033593575933, 
+    0.12215506287920677, 0.23993949201268056, 0.4019144709381388, 
+    0.3183907920796311, 0.3913899537158143, 0.0049291176692496394, 
+    0.3676716852255129
+]
+AFB_aerr = [
+    0.008421043319078612, 0.03195845613292291, 0.029395083110202395, 
+    0.03961889843177776, 0.04728136705066239, 0.030140936643680404, 
+    0.03393144831695434, 0.023627117754196975, 0.02805838529981395, 
+    0.03223029184299107
+]
+
+
+with open('tmp/af_fitting.pkl', 'rb') as f:
+    fits = pickle.load(f)
+
+fig, ax = plt.subplots(1, 2, constrained_layout=True)
+
+ax[0].errorbar(range(10), fits['fls'], yerr=fits['fl_errs'], fmt='k.', ms=5)
+ax[0].errorbar(range(10), FL_aval, yerr=FL_aerr, fmt='r.', ms=5)
+ax[0].set(xlabel='Bin number', ylabel=r'$F_L$', xticks=range(10))
+ax[0].grid()
+# ax[0].plt.xticks(np.arange(min(x), max(x)+1, 1.0))
+
+ax[1].errorbar(range(10), fits['afbs'], yerr=fits['afb_errs'], fmt='k.', ms=5)
+ax[1].errorbar(range(10), AFB_aval, yerr=AFB_aerr, fmt='r.', ms=5)
+ax[1].grid()
+ax[1].set(xlabel='Bin number', ylabel=r'$A_{FB}$', xticks=range(10))
+
+fig.suptitle('Our fit (black), "actual" data (red)', size=17)
+
+plt.show()
+
