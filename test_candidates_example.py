74b13edc3e7b79f8bc56f5f1ec0f249ebd3feba2
Marek Cottingham <36125218+marek-cottingham@users.noreply.github.com>
2022-02-28 22:06:04 +0000

    Fixed / flagged broken imports

--- test_candidates_example.py
+++ test_candidates_example.py
@@ -1,56 +1,11 @@
-from core import load_file, RAWFILES
+from core import RAWFILES
 import pandas as pd
-from ES_functions.Compiled import q2_resonances, Kstar_inv_mass, B0_vertex_chi2, final_state_particle_IP, B0_IP_chi2, FD, DIRA, Particle_ID, selection_all
-# import ml_load
-import numpy as np
-
-#%%
-def test_false_true_negative_positive(test_dataset, sig_prob, threshold) -> dict:
-    # Jiayang
-
-    x = test_dataset['category'].to_numpy()
-
-    x_mask_0 = x == 0
-    x_mask_1 = x == 1
-    prb_mask_pos = sig_prob >= threshold
-    prb_mask_neg = sig_prob < threshold
-
-    signal = np.count_nonzero(x_mask_1)
-    background = np.count_nonzero(x_mask_0)
-    true_positive =  np.count_nonzero(np.logical_and(x_mask_1, prb_mask_pos))
-    false_negative = np.count_nonzero(np.logical_and(x_mask_1, prb_mask_neg))
-    false_positive = np.count_nonzero(np.logical_and(x_mask_0, prb_mask_pos))
-    true_negative =  np.count_nonzero(np.logical_and(x_mask_0, prb_mask_neg))
-    
-    # rates
-    tpr = true_positive / signal
-    fpr = false_positive / background
-
-    fnr = false_negative / signal
-    tnr = true_negative / background
-
-    return {
-        'true-positive': tpr,
-        'false-positive': fpr,
-        'true-negative': tnr,
-        'false-negative': fnr,
-        'signal': signal,
-        'background': background,
-        'n-signal-accept': signal * tpr,
-        'n-background-accept': background * fpr,
-    }
-
-def test_sb(test_dataset, sig_prob, threshold):
-    # Jiayang
-
-    output = test_false_true_negative_positive(test_dataset, sig_prob, threshold)
-
-    S = output['signal'] * output['true-positive']
-    B = output['background'] * output['false-positive']
-    if S+B == 0:
-        return 0
-    metric = S/np.sqrt(S+B)
-    return metric
+from ES_functions.Compiled import (
+    q2_resonances, Kstar_inv_mass, B0_vertex_chi2, final_state_particle_IP, 
+    B0_IP_chi2, FD, DIRA, Particle_ID, selection_all
+)
+from ml_tools import test_false_true_negative_positive, test_sb
+from ml_main import load_train_validate_test, combine_signal_background, concat_datasets
 
 def test_candidate_true_false_positive_negative(test_data, selection_method=q2_resonances):
     
@@ -63,32 +18,32 @@ def test_candidate_true_false_positive_negative(test_data, selection_method=q2_r
     sig_prob = test_res_data['pred'].to_numpy()
     
     dict1=test_false_true_negative_positive(test_res_data, sig_prob ,0.5)
-    dict2={'SB Metric':test_sb(test_res_data, sig_prob ,0.5)}#find SB metric and make it a dictionary
-    both_dicts = {**dict1, **dict2}#merge dictionaries
+    #find SB metric and make it a dictionary
+    dict2={'SB Metric':test_sb(test_res_data, sig_prob ,0.5)}
+    #merge dictionaries
+    both_dicts = {**dict1, **dict2}
     return both_dicts
     
 
 #%%
 if __name__ == '__main__':
-    #Load signal and non-signal, then loop through selection cuts including "selection_all" which is all cuts
-    signal = load_file(RAWFILES.SIGNAL)
-    #signal=pd.read_pickle('data/signal.pkl')
+    # Load signal and non-signal, then loop through selection cuts including 
+    # "selection_all" which is all cuts
+    signal = load_train_validate_test(RAWFILES.SIGNAL, validate_size=0)
+    background = concat_datasets([
+        load_train_validate_test(file, validate_size=0) for file in RAWFILES.peaking_bks])
+    train, test = combine_signal_background(signal, background)
 
-    non_signal = []
-    for file in RAWFILES.peaking_bks:
-        data = load_file(file)
-        #data = pd.read_pickle(f'data/{file}')
-        non_signal.append(data)
-    non_signal = pd.concat(non_signal)
-    test_data = ml_tools.ml_combine_signal_bk(signal, non_signal)
-
-    train, test = ml_load.get_train_test_for_all_peaking_bks()
-
-    funclist=[q2_resonances, Kstar_inv_mass, B0_vertex_chi2, final_state_particle_IP, B0_IP_chi2, FD, DIRA,] #Particle_ID, selection_all]
+    funclist=[q2_resonances, Kstar_inv_mass, B0_vertex_chi2, final_state_particle_IP, 
+        B0_IP_chi2, FD, DIRA, Particle_ID, selection_all]
     output={}
     for func in funclist:
         print('Testing selector', func.__name__)
         output[func.__name__] = (test_candidate_true_false_positive_negative(test,func))
     for key in output:
-        print(f'{key} | tpr: {output[key]["true-positive"]:.4g}, fpr: {output[key]["false-positive"]:.4g}, sb: {output[key]["SB Metric"]:4g}')
-    #30/01/2022: Error with Particle_ID func (and therefore selection_all func) - possibly due to ml_combine_signal_bk func used in line 9?
+        print(
+            (
+                f'{key} | tpr: {output[key]["true-positive"]:.4g}, '
+                f'fpr: {output[key]["false-positive"]:.4g}, sb: {output[key]["SB Metric"]:4g}'
+            )
+        )
