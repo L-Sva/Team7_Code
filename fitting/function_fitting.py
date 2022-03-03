aa3baa59cd06eb50033f4eb33056ea3a1364eed9
Marek Cottingham <36125218+marek-cottingham@users.noreply.github.com>
2022-03-03 15:56:13 +0000

    Function fitting modified imports

---
+++ fitting/function_fitting.py
@@ -0,0 +1,51 @@
+import numpy as np
+from scipy.integrate import quad
+
+from functions import acceptance_function
+
+def raw_d2(ctl, fl, afb, _bin, q_norm, params_dict):
+    c2tl = 2 * ctl ** 2 - 1
+    acceptance = acceptance_function(q_norm[_bin], ctl, params_dict)
+    scalar_array = 3/8 * (3/2 - 1/2 * fl + 1/2 * c2tl * (1 - 3 * fl) +
+                   8/3 * afb * ctl) * acceptance
+    return scalar_array
+
+def d2gamma_p_d2q2_dcostheta(fl, afb, ctl, _bin, q_norm, params_dict):
+    """
+    Returns the pdf defined above
+    :param fl: f_l observable
+    :param afb: a_fb observable
+    :param cos_theta_l: cos(theta_l)
+    """
+    # normalise array to account for the non-unity acceptance function
+    normalised_scalar_array = raw_d2(
+        ctl, fl, afb, _bin, q_norm, params_dict).squeeze() / \
+    quad(raw_d2, -1, 1, args = (fl, afb, _bin, q_norm, params_dict))[0]
+
+    return normalised_scalar_array
+
+def log_likelihood(df, q_norm, params_dict, fl, afb, _bin):
+    """
+    Returns the negative log-likelihood of the pdf defined above
+    :param df: q² binned dataset
+    :param q_norm: rescaled (between [-1, +1]) q² values to
+    evaluate acceptance function at
+    :param params_dict: dictionary with required values for calculating the
+    acceptance function. Needs keys: P and c, automatically added when
+    find_acceptance.py is run.
+    :param fl: f_l observable
+    :param afb: a_fb observable
+    :param _bin: number of the bin to fit
+    """
+
+    _bin = int(_bin) # make sure index is an integer
+
+    bin_data = df[str(_bin)]
+    ctl = bin_data['costhetal'].to_numpy()
+    normalised_scalar_array = d2gamma_p_d2q2_dcostheta(
+        fl=fl, afb=afb, ctl=ctl, _bin=_bin,
+        q_norm=q_norm, params_dict=params_dict
+    )
+    with np.errstate(invalid='ignore'): # ignore 'invalid log' message
+        NLL = -np.log(normalised_scalar_array).sum(-1)
+    return NLL
--- fitting/main.py
+++ fitting/main.py
@@ -8,56 +8,9 @@ from functools import partial
 from Team7_Code.core import load_file, RAWFILES
 from Team7_Code.ES_functions.Compiled import selection_all
 from functions import q2_binned, acceptance_function, q2bins, rescale_q2
+from function_fitting import log_likelihood
 
 plt.rcParams['font.size'] = 18
-
-def raw_d2(ctl, fl, afb, _bin, q_norm, params_dict):
-    c2tl = 2 * ctl ** 2 - 1
-    acceptance = acceptance_function(q_norm[_bin], ctl, params_dict)
-    scalar_array = 3/8 * (3/2 - 1/2 * fl + 1/2 * c2tl * (1 - 3 * fl) +
-                   8/3 * afb * ctl) * acceptance
-    return scalar_array
-
-def d2gamma_p_d2q2_dcostheta(fl, afb, ctl, _bin, q_norm, params_dict):
-    """
-    Returns the pdf defined above
-    :param fl: f_l observable
-    :param afb: a_fb observable
-    :param cos_theta_l: cos(theta_l)
-    """
-    # normalise array to account for the non-unity acceptance function
-    normalised_scalar_array = raw_d2(
-        ctl, fl, afb, _bin, q_norm, params_dict).squeeze() / \
-    quad(raw_d2, -1, 1, args = (fl, afb, _bin, q_norm, params_dict))[0]
-
-    return normalised_scalar_array
-
-def log_likelihood(df, q_norm, params_dict, fl, afb, _bin):
-    """
-    Returns the negative log-likelihood of the pdf defined above
-    :param df: q² binned dataset
-    :param q_norm: rescaled (between [-1, +1]) q² values to
-    evaluate acceptance function at
-    :param params_dict: dictionary with required values for calculating the
-    acceptance function. Needs keys: P and c, automatically added when
-    find_acceptance.py is run.
-    :param fl: f_l observable
-    :param afb: a_fb observable
-    :param _bin: number of the bin to fit
-    """
-
-    _bin = int(_bin) # make sure index is an integer
-
-    bin_data = df[str(_bin)]
-    ctl = bin_data['costhetal'].to_numpy()
-    normalised_scalar_array = d2gamma_p_d2q2_dcostheta(
-        fl=fl, afb=afb, ctl=ctl, _bin=_bin,
-        q_norm=q_norm, params_dict=params_dict
-    )
-    with np.errstate(invalid='ignore'): # ignore 'invalid log' message
-        NLL = -np.log(normalised_scalar_array).sum(-1)
-    return NLL
-
 
 if __name__=='__main__':
     with open('tmp/acceptance_coeff.pkl', 'rb') as f:
