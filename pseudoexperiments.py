5fa078380054d51471e9bbbdaf26291a511fcb9d
Marek Cottingham <36125218+marek-cottingham@users.noreply.github.com>
2022-03-14 20:39:49 +0000

    Pseudoexperiments change

--- pseudoexperiments.py
+++ pseudoexperiments.py
@@ -1,7 +1,9 @@
+from dataclasses import dataclass
 from functools import partial
 import os
 import pickle
-from ES_functions.Compiled import selection_all
+from typing import List
+from ES_functions.Compiled import q2_resonances, selection_all
 from core import RAWFILES, ensure_dir, load_file
 from fitting.function_fitting import log_likelihood
 from ml_selector import remove_all_bk
@@ -20,31 +22,32 @@ def cached_compute_pseudoexperiments(data,cache_file_path,n=50):
         with open(cache_file_path, 'wb') as file:
             pseudoexperiments = pickle.load(file)
 
+@dataclass
+class fl_afb_fit():
+    fl_best: List = None
+    fl_best_err: List = None
+    afb_best: List = None
+    afb_best_err: List = None
+    likelihood_best: List = None
+
 class Experiment():
     def __init__(self,data):
-        self.data = data
-        self.acceptance_params = None
-        self.fl_best = None
-        self.fl_best_err = None
-        self.afb_best = None
-        self.afb_best_err = None
-        self.likelihood_best = None
-
+        self.data_with_resonances = data
+        self.data_without_resonasces, _ = q2_resonances(self.data_with_resonances)
+        
     def set_acceptance_params(self,params):
         self.acceptance_params = params
     
-    def fit_ab_and_fl(self):
-        assert self.acceptance_params is not None
+    def fit_ab_and_fl(self, acceptance_params):
 
         df_log_likelihood = partial(
-            log_likelihood, self.data, self.acceptance_params
+            log_likelihood, self.data, acceptance_params
         )
 
         fls, fl_errs = [], []
         afbs, afb_errs = [], []
 
         for bin_number in range(10):
-            
             m = Minuit(df_log_likelihood, fl=0.0, afb=0.0, _bin=bin_number)
             # fixing the bin number as we don't want to optimize it
             m.fixed['_bin'] = True  
@@ -57,13 +60,10 @@ class Experiment():
             fl_errs.append(m.errors[0])
             afb_errs.append(m.errors[1])
 
-        self.fl_best = fls
-        self.fl_best_err = fl_errs
-        self.afb_best = afbs
-        self.afb_best_err = afb_errs
-        self.likelihood_best = [
+        return fl_afb_fit(fls, fl_errs, afbs, afb_errs, [
             df_log_likelihood(fl, afb, bin) for fl, afb, bin in zip(fls,afbs,range(10))
-        ]
+        ])
+        
 
     def make_and_fit_pseudoexperiments_acceptance(self,pseudodatasets):
         self.pseudoexperiments = [
